d2330bcf2c403877a2acfd286f1f3125d9c6f1b3
9000Noemi <noemi.martinc@gmail.com>
2024-09-14 10:15:38 +0200

    Exersize1 done

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,1 +1,30 @@
-console.log("COMENZANDO")
+
+ const user = {
+    name: 'Han',
+    surname: 'Solo',
+    subjects:[
+        {
+        name: 'Node.js',
+        date: '2024-12-01'
+        },
+        {
+        name: 'Git',
+        date: '2025-01-15'   
+        },
+        {
+        name: 'React',
+        date: '2025-03-03'
+        }
+    ],
+    active_search: false
+    }
+
+
+for (let i=0; i < user.subjects.length; i++){
+    if (user.subjects[i].name === 'React'){
+        console.log(user.subjects[i].date)
+        break;
+    }
+}
+
+
